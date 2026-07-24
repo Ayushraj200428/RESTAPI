b3384da57d167b9cf05f9221852a7a87acd54e27
Ayush Raj <rajayush03281@gmail.com>
2026-07-25 01:32:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const users = require("./MOCK_DATA.json")
+const app = express();
+const port = 8000
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+
+app.route('/api/users/:id').get((req,res)=>{
+    const id = Number(req.params.id);
+    const user=users.find((user)=> user.id===id);
+    return res.json(user);
+    })
+    .patch((req,res)=>{
+        return res.json({status:"pending"})
+    })
+    .delete((req,res)=>{
+        return res.json({status:"pending"})
+    })
+
+
+app.post('/api/users', (req,res)=>{
+    return res.json({status:"pending"});
+});
+
+app.patch("/api/users/:id",(req,res)=>{
+    return res.json({status:"pending"});
+});
+
+app.listen(port, ()=> console.log(`server started at port:"${port}`))
