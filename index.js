bf953d05bb496878e79f194fa7fbab3ed2283fdf
Ayush Raj <rajayush03281@gmail.com>
2026-07-25 17:10:38 +0530

    completed project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const users = require("./MOCK_DATA.json")
+const fs = require('fs')
 const app = express();
 const port = 8000
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.get('/api/users',(req,res)=>{
     return res.json(users);
@@ -12,20 +16,14 @@ app.route('/api/users/:id').get((req,res)=>{
     const user=users.find((user)=> user.id===id);
     return res.json(user);
     })
-    .patch((req,res)=>{
-        return res.json({status:"pending"})
-    })
-    .delete((req,res)=>{
-        return res.json({status:"pending"})
-    })
-
 
 app.post('/api/users', (req,res)=>{
-    return res.json({status:"pending"});
-});
-
-app.patch("/api/users/:id",(req,res)=>{
-    return res.json({status:"pending"});
+    const body=req.body;
+    users.push({...body, id: users.length+1})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data)=>{
+        return res.json({status:"success", id:users.length});
+    })
+    
 });
 
 app.listen(port, ()=> console.log(`server started at port:"${port}`))
